303dc8a8aac83537b84c7911b7906e868f7c700d
Shahjahan Rasel <shadow2burn@gmail.com>
2023-08-16 10:14:28 -0500

    Customer and Product service updated

--- src/main/java/com/kinbenaki/ordermicroservice/Service/CustomerService.java
+++ src/main/java/com/kinbenaki/ordermicroservice/Service/CustomerService.java
@@ -1,11 +1,13 @@
 package com.kinbenaki.ordermicroservice.Service;
 
+import com.kinbenaki.ordermicroservice.Exception.ProductNotFoundException;
 import com.kinbenaki.ordermicroservice.Model.Customer;
 import com.kinbenaki.ordermicroservice.Repository.CustomerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class CustomerService {
@@ -23,6 +25,25 @@ public class CustomerService {
     public Customer createCustomer(Customer customer) {
         return customerRepository.save(customer);
     }
+
+    public Customer updateCustomer(Long customerId, Customer updatedCustomer) {
+        Optional<Customer> optionalCustomer = customerRepository.findById(customerId);
+
+        if (optionalCustomer.isPresent()) {
+            Customer existingCustomer = optionalCustomer.get();
+            existingCustomer.setAddress(updatedCustomer.getAddress());
+            existingCustomer.setEmail(updatedCustomer.getEmail());
+            existingCustomer.setPhone(updatedCustomer.getPhone());
+            existingCustomer.setLastName(updatedCustomer.getLastName());
+            existingCustomer.setFirstName(updatedCustomer.getFirstName());
+
+            return customerRepository.save(existingCustomer);
+        } else {
+            // Handle case where product with given ID is not found
+            throw new ProductNotFoundException("Product not found with ID: " + customerId);
+        }
+    }
+
 
     public void deleteCustomer(Long customerId) {
         customerRepository.deleteById(customerId);
--- src/main/java/com/kinbenaki/ordermicroservice/Service/ProductService.java
+++ src/main/java/com/kinbenaki/ordermicroservice/Service/ProductService.java
@@ -1,11 +1,13 @@
 package com.kinbenaki.ordermicroservice.Service;
 
+import com.kinbenaki.ordermicroservice.Exception.ProductNotFoundException;
 import com.kinbenaki.ordermicroservice.Model.Product;
 import com.kinbenaki.ordermicroservice.Repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ProductService {
@@ -22,6 +24,23 @@ public class ProductService {
 
     public Product createProduct(Product product) {
         return productRepository.save(product);
+    }
+
+    public Product updateProduct(Long productId, Product updatedProduct) {
+        Optional<Product> optionalProduct = productRepository.findById(productId);
+
+        if (optionalProduct.isPresent()) {
+            Product existingProduct = optionalProduct.get();
+            existingProduct.setProductName(updatedProduct.getProductName());
+            existingProduct.setDescription(updatedProduct.getDescription());
+            existingProduct.setUnitPrice(updatedProduct.getUnitPrice());
+            existingProduct.setAvailableStockQuantity(updatedProduct.getAvailableStockQuantity());
+
+            return productRepository.save(existingProduct);
+        } else {
+            // Handle case where product with given ID is not found
+            throw new ProductNotFoundException("Product not found with ID: " + productId);
+        }
     }
 
     public void deleteProduct(Long productId) {
