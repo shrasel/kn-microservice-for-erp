b13fcf02678f25c66785f9cde039684e660d0630
Shahjahan Rasel <shadow2burn@gmail.com>
2023-08-15 23:13:32 -0500

    Initial Commit

---
+++ src/main/java/com/kinbenaki/ordermicroservice/Model/Order.java
@@ -0,0 +1,22 @@
+package com.kinbenaki.ordermicroservice.Model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Entity
+@Table(name = "orders")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+//    @OneToMany(mappedBy = "Order")
+//    private List<Item> items;
+
+}
